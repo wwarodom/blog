7c4c7494af2716e9e1110fdd5eda9dfeb77a0c23
warodom.w <warodom.w>
2017-03-20 15:43:28 +0700

    session update

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -6,7 +6,16 @@ use Illuminate\Http\Request;
 
 class CartController extends Controller
 {
-    public function orderProduct() {
+	public function index() {
+		session(['cake' => '49.90']);
+		session()->put('foo','Foo bar value');
+	 	return view('cart.index');
+	}
+
+    public function orderProduct() {    	
     	echo session('cake');
+    	session()->forget('cake');
+    	echo "<br/><br/>	";
+    	var_dump(session()->all());
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@ Route::get('/example2', function() {
 	return view('example.example2');
 });
 
-Route::get('/cart', function() {
-	session(['cake' => '49.90']);
-	return view('cart.index');
-});
+Route::get('/cart','CartController@index');
 
 Route::get('/cart/orderProduct','CartController@orderProduct');
